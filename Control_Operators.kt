7ca72e175c501bd5889ed6df6ea6e8bc0be0005d
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-18 00:43:58 +0530

    if, else as an expression

--- Control_Operators.kt
+++ Control_Operators.kt
@@ -7,12 +7,18 @@ fun main() {
 //    else println("$input is Negative Number")
 
     if (input > 0) println("$input is Positive Number")
-    else if (input<0) println("$input is Negative Number")
+    else if (input < 0) println("$input is Negative Number")
     else println("$input is Zero")
 
     println("Enter your second Integer Number")
     val input2 = readln().toInt()
-    if (input>input2) println("$input is bigger than $input2")
-    else if (input<input2) println("$input is Small than $input2")
+    if (input > input2) println("$input is bigger than $input2")
+    else if (input < input2) println("$input is Small than $input2")
     else println("$input is equal to $input2")
+
+    val max =
+        if (input > input2) input
+        else if (input < input2) input2
+        else println("Both number is equal")
+    println("$max is max")
 }
