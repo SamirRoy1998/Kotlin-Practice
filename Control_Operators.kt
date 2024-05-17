e2a986d5b4576ab771865892f67b172b0b955729
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-18 00:10:24 +0530

    more with if, else

--- Control_Operators.kt
+++ Control_Operators.kt
@@ -2,7 +2,11 @@ package practice_kotlin
 
 fun main() {
     print("Enter a Integer Number: ")
-    val input= readln().toInt()
-    if (input>0) println("$input is Positive Number")
-    else println("$input is Negative Number")
+    val input = readln().toInt()
+//    if (input > 0) println("$input is Positive Number")
+//    else println("$input is Negative Number")
+
+    if (input > 0) println("$input is Positive Number")
+    else if (input<0) println("$input is Negative Number")
+    else println("$input is Zero")
 }
