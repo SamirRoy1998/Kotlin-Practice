1b58ae902ba5fadb23f8547e990b6a4da5c93455
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-17 20:11:41 +0530

    just added

---
+++ ps1.kt
@@ -0,0 +1,11 @@
+package practice_kotlin
+
+fun main() {
+    println("Hello World.")
+    val i = 10
+    val name = "Samir"
+
+    println(i.toString() + name)
+    println("$i $name")
+    println(name + i)
+}
