67a2542275f2b05d3681ab948dd323959d09dfc6
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-17 23:38:03 +0530

    some experiments

--- Taking_UserInput.kt
+++ Taking_UserInput.kt
@@ -1,2 +1,20 @@
 package practice_kotlin
 
+fun main() {
+    println("Enter your name: ")
+    val a = readLine() //nullable
+    val b = readln().toInt() //non-nullable if null throw run time exceptions
+    println(a)
+    println(b*2)
+
+    print("Age: ")
+    val age = readln().toInt()
+    print("Id: ")
+    val id = readln()
+    print("Name: ")
+    val name = readln()
+
+    println(id)
+    println(name)
+    println(age)
+}
