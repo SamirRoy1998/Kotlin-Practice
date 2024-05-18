29b24edddf62a7e404efa37a72c2520a521057fa
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-18 17:15:58 +0530

    done

--- Day_Number.kt
+++ Day_Number.kt
@@ -1,2 +1,16 @@
 package practice_kotlin
 
+fun main() {
+    println("Enter a number (1-7)")
+    val dayNum = readln().toInt()
+    when (dayNum) {
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+        else -> println("Enter correct number")
+    }
+}
