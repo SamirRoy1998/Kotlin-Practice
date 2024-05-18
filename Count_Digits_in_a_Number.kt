89d0e5e25232b4135f1bd6900d601b7d2e20ff2d
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-19 02:11:39 +0530

    done

--- Count_Digits_in_a_Number.kt
+++ Count_Digits_in_a_Number.kt
@@ -1,2 +1,12 @@
 package practice_kotlin
 
+fun main() {
+    print("Enter a value: ")
+    var input = readln().toInt()
+    var count = 0
+    while (input > 0) {
+        input = input / 10
+        count++
+    }
+    println("you entered value has $count-digit.")
+}
