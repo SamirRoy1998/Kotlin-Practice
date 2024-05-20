dd758964a1e75a969133d9cdd4f73a943ba93811
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-20 15:35:53 +0530

    done

--- Reverse_Input_Number.kt
+++ Reverse_Input_Number.kt
@@ -1,2 +1,17 @@
 package practice_kotlin
 
+fun reverseNumber(number: Int): Int {
+    var n = number
+    var r = 0
+    while (n != 0) {
+        val digit = n % 10
+        r = r * 10 + digit
+        n /= 10
+    }
+    return r
+}
+
+fun main() {
+    val input = readln().toInt()
+    println(reverseNumber(input))
+}
