b9cba4978d2ca4e6e7442e684bbf1cdbf52bd356
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-20 17:35:16 +0530

    done

--- Check_Prime_Number.kt
+++ Check_Prime_Number.kt
@@ -1,2 +1,16 @@
 package practice_kotlin
 
+fun checkPrime(number: Int): Boolean {
+    var factorCount = 0
+    for (i in 1..number) {
+        if (number % i == 0) {
+            factorCount++
+        }
+    }
+    return factorCount==2
+}
+
+fun main() {
+    val input = readln().toInt()
+    println(checkPrime(input))
+}
