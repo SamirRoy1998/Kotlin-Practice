c4821b58da75d0f45521a129b66ca0fa99c79c68
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-20 16:29:18 +0530

    done

--- Cube_Of_Each_Digits_In_A_Number.kt
+++ Cube_Of_Each_Digits_In_A_Number.kt
@@ -1,5 +1,17 @@
 package practice_kotlin
 
+fun sumOfCubeOfEachDigit(num: Int): Int {
+    var n = Math.abs(num) //Math.abs() used for treat negative number as a positive number
+    var sum = 0
+    while (n != 0) {
+        val digit = n % 10
+        sum = sum + digit * digit * digit
+        n /= 10
+    }
+    return sum
+}
+
 fun main() {
-    
+    val input = readln().toInt()
+    println(sumOfCubeOfEachDigit(input))
 }
