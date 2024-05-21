2e84a6fe745425657ab766ff5f3bb1837bfb0540
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-21 21:59:27 +0530

    Example1 added more things to do

--- Methods_And_Functions.kt
+++ Methods_And_Functions.kt
@@ -1,2 +1,22 @@
 package practice_kotlin
 
+fun test1() {
+    println("I'm a function, coz I don't have a any class")
+}
+
+class Example1 {
+    fun test1() {
+        println("I'm a method, coz I have a any class")
+    }
+
+    fun main() {
+        val ex = Example1()
+        ex.test1()          //  <- Bug That have to fix
+    }
+}
+
+fun main() {
+    val ex = Example1()     //  <- Object a Create to call method
+    test1()                 //  <- Calling a function
+    ex.test1()              //  <- Calling a method
+}
