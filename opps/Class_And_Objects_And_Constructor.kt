270b5423f881d6c24a6421ad65551764d832df76
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-22 21:43:48 +0530

    Another Class with parameterized Constructor added

--- opps/Class_And_Objects_And_Constructor.kt
+++ opps/Class_And_Objects_And_Constructor.kt
@@ -25,6 +25,36 @@ class Student {
     }
 }
 
+//-------------------------------------------------------------------------------------
+class AdvStudent(
+    private var name: String = "",
+    private var sirName: String = "",
+    private var age: Int = 0,
+    private var id: Int = 0,
+    private var district: String = "",
+    private var city: String = "",
+    private var area: String = "",
+    private var colony: String = "",
+    private var landMark: String = "",
+    private var houseNo: String,
+) {
+
+    private val address: String get() = "$houseNo, $colony, $landMark, $area, $city, $district"
+    private val fullName get() = "$name $sirName"
+
+    init {
+        println("---Advanced Student Class---")
+    }
+
+    fun printDetails() {
+        println("Id: $id")
+        println("Full Name: $fullName")
+        println("Age: $age")
+        println("Address: $address")
+        println("----------------------------")
+    }
+}
+
 fun main() {
     val st1 = Student()
     st1.name = "Samir"
@@ -39,4 +69,18 @@ fun main() {
     st1.houseNo = "N/355/D"
 
     st1.printDetails()
+//--------------------------------------------------------------
+    val ast1 = AdvStudent(
+        "Samir",
+        "Roy",
+        25,
+        5124,
+        "Bongaigaon",
+        "New Bongaigaon",
+        "Dangtola",
+        "Central Colony",
+        "Srimanta Sankardev Namghar",
+        "N/355/D"
+    )
+    ast1.printDetails()
 }
