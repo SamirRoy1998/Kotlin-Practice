7d68e158605025824a2935c8ce6a1df4f2e66673
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-23 19:02:02 +0530

    Father and Son class added some method and property override

--- opps/Override_Example.kt
+++ opps/Override_Example.kt
@@ -1,2 +1,34 @@
 package practice_kotlin.opps
 
+open class SukhranjanLalTripathi {
+    open val name = "Sukhranjan Lal Tripathi"
+    val land = "2000 sqft"
+    open fun house() {
+        println("One story house, House owner is $name covered in $land")
+    }
+    open fun landOwner() {
+        println("Land owner is $name")
+    }
+}
+
+class MotiLalTripathi : SukhranjanLalTripathi() {
+    override val name = "Moti Lal Tripathi"
+    override fun house() {
+        println("Five story building, Building owner is $name covered in $land")
+    }
+    override fun landOwner() {
+        println("Land owner is ${super.name}")
+    }
+}
+
+fun main() {
+    val skLal = SukhranjanLalTripathi()
+    skLal.house()
+    skLal.landOwner()
+    println("=========================================================================================================================")
+
+    val mlal = MotiLalTripathi()
+    mlal.house()
+    mlal.landOwner()
+    println("=========================================================================================================================")
+}
