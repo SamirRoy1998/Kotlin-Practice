175da671705fd969b12025c9e02468db4cc63522
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-23 21:32:38 +0530

    Abstract Animal class added

--- opps/Abstract_Classes.kt
+++ opps/Abstract_Classes.kt
@@ -1,2 +1,14 @@
 package practice_kotlin.opps
 
+abstract class Animal{
+    fun eat(){
+        println("Animal is eating")
+    }
+    fun sleep(){
+        println("Animal is sleeping")
+    }
+    fun breathe(){
+        println("Animal is breathing")
+    }
+    abstract fun roam()
+}
