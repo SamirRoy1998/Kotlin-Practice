2cf505458ec59c70cf672cd014981d01e40383e7
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-23 20:06:57 +0530

    Grandson added and override all the methods from parent class

--- opps/Override_Example.kt
+++ opps/Override_Example.kt
@@ -2,22 +2,36 @@ package practice_kotlin.opps
 
 open class SukhranjanLalTripathi {
     open val name = "Sukhranjan Lal Tripathi"
-    val land = "2000 sqft"
+    open val land = "2000 sqft"
     open fun house() {
         println("One story house, House owner is $name covered in $land")
     }
+
     open fun landOwner() {
         println("Land owner is $name")
     }
 }
 
-class MotiLalTripathi : SukhranjanLalTripathi() {
+open class MotiLalTripathi : SukhranjanLalTripathi() {
     override val name = "Moti Lal Tripathi"
     override fun house() {
         println("Five story building, Building owner is $name covered in $land")
     }
+
     override fun landOwner() {
         println("Land owner is ${super.name}")
+    }
+}
+
+class JohnTripathi : MotiLalTripathi() {
+    override val name = "John Tripathi"
+    override val land = "5000 sqft"
+    override fun house() {
+        println("Ten story mansion, Mansion owner is $name covered in $land")
+    }
+
+    override fun landOwner() {
+        println("Land owner is $name")
     }
 }
 
@@ -30,5 +44,10 @@ fun main() {
     val mlal = MotiLalTripathi()
     mlal.house()
     mlal.landOwner()
+    println("=========================================================================================================================")
+
+    val jt = JohnTripathi()
+    jt.house()
+    jt.landOwner()
     println("=========================================================================================================================")
 }
