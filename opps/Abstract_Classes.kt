2aa7c470eb96ef6a240ea81150045f2c618e06c1
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-23 21:38:49 +0530

    Cat and Dog two concrete class added

--- opps/Abstract_Classes.kt
+++ opps/Abstract_Classes.kt
@@ -1,14 +1,42 @@
 package practice_kotlin.opps
 
-abstract class Animal{
-    fun eat(){
+abstract class Animal {
+    open fun eat() {
         println("Animal is eating")
     }
-    fun sleep(){
+
+    fun sleep() {
         println("Animal is sleeping")
     }
-    fun breathe(){
+
+    fun breathe() {
         println("Animal is breathing")
     }
+
     abstract fun roam()
+    abstract fun speak()
+}
+
+class Cat : Animal() {
+    override fun roam() {
+        println("Cat is walking")
+    }
+
+    override fun speak() {
+        println("Meow Meow Meow")
+    }
+}
+
+class Dog : Animal() {
+    override fun eat() {
+        println("Dog is eating")
+    }
+
+    override fun roam() {
+        println("Dog is running")
+    }
+
+    override fun speak() {
+        println("Bow wow wow wow")
+    }
 }
