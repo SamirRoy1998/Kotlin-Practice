980f8498cc9af784a61b61720a1e0616ceca7b69
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-24 22:54:50 +0530

    data class example added

--- opps/Data_Classes.kt
+++ opps/Data_Classes.kt
@@ -1,2 +1,30 @@
 package practice_kotlin.opps
 
+class Normal(var name:String, var age:Int)
+data class DataClass(var name:String, var age:Int)
+
+fun main() {
+    val n = Normal("Normal", 20)
+    val d = DataClass("DataClass", 20)
+    println("n: $n")
+    println("n.hashCode(): ${n.hashCode()}")
+    println("n.equals(d): ${n.equals(d)}")
+    println("n.toString(): ${n.toString()}")
+//    println(n.component1())     -> error
+//    println(n.component2())     -> error
+//    println(n.copy())           -> error
+//    println(n.copy(age = 30))   -> error
+//    println(n.component1())     -> error
+//    println(n.component2())     -> error
+    println("---------------------------------------------------")
+    println("d: $d")
+    println("d.hashCode(): ${d.hashCode()}")
+    println("d.equals(n): ${d.equals(n)}")
+    println("d.toString(): ${d.toString()}")
+    println("d.component1(): ${d.component1()}")
+    println("d.component2(): ${d.component2()}")
+    println("d.copy(): ${d.copy()}")
+    println("d.copy(age = 30): ${d.copy(age = 30)}")
+    println("d.component1(): ${d.component1()}")
+    println("d.component2(): ${d.component2()}")
+}
