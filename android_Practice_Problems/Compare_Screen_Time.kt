dca86193223313da9f7e7a57ae156a578b3f8c94
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-30 20:44:09 +0530

    added a function that shows true or false

--- android_Practice_Problems/Compare_Screen_Time.kt
+++ android_Practice_Problems/Compare_Screen_Time.kt
@@ -1,2 +1,12 @@
 package practice_kotlin.android_Practice_Problems
 
+fun main() {
+    val timeSpentToday = 400
+    val timeSpentYesterday = 300
+    val result = isMoreThanYesterday(timeSpentToday, timeSpentYesterday)
+    println(result)
+}
+
+fun isMoreThanYesterday(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
+    return timeSpentToday > timeSpentYesterday
+}
