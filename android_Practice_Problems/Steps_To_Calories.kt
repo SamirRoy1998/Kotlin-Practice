342fa97d51411560ebd96c49e7e96aa27abaedb1
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-30 19:46:39 +0530

    google problem

--- android_Practice_Problems/Steps_To_Calories.kt
+++ android_Practice_Problems/Steps_To_Calories.kt
@@ -1,2 +1,7 @@
 package practice_kotlin.android_Practice_Problems
 
+fun main() {
+    val steps = 4000
+    val caloriesBurned = pedometerStepsToCalories(steps);
+    println("Walking $steps steps burns $caloriesBurned calories")
+}
