8905fab6546707113d2a4ff73ebd6c9af98c7518
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-30 21:00:11 +0530

    made a function to display weather reports

--- android_Practice_Problems/Display_Weather_Report.kt
+++ android_Practice_Problems/Display_Weather_Report.kt
@@ -1,2 +1,18 @@
 package practice_kotlin.android_Practice_Problems
 
+fun main() {
+    val city = readln()
+    val lowTemperature = readln()
+    val highTemperature= readln()
+    val chanceOfRain = readln()
+
+    weatherReport(city,lowTemperature,highTemperature,chanceOfRain)
+    weatherReport("Bongaigaon","20","45","20")
+}
+
+fun weatherReport(city: String, lowTemperature: String, highTemperature: String, chanceOfRain: String) {
+    println("City: $city")
+    println("Low temperature: $lowTemperature, High temperature: $highTemperature")
+    println("Chance of rain: $chanceOfRain%")
+    println()
+}
