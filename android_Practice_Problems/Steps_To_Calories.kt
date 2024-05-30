6c46f9c09aa4a0c80aabc648184d3eb2c998ccc6
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-30 19:50:50 +0530

    a function created for caloriesBurned

--- android_Practice_Problems/Steps_To_Calories.kt
+++ android_Practice_Problems/Steps_To_Calories.kt
@@ -2,6 +2,12 @@ package practice_kotlin.android_Practice_Problems
 
 fun main() {
     val steps = 4000
-    val caloriesBurned = pedometerStepsToCalories(steps);
+    val caloriesBurned = pedometerStepsToCalories(steps)
     println("Walking $steps steps burns $caloriesBurned calories")
+}
+
+fun pedometerStepsToCalories(steps: Int): Double {
+    val caloriesBurnedEachSteps = 0.04
+    val totalCategoryBurned = steps * caloriesBurnedEachSteps
+    return totalCategoryBurned
 }
