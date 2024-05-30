c93b33121f7c14cf76a5b2c2221280f1ab5158d1
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-30 19:39:48 +0530

    google problem

--- android_Practice_Problems/Display_Alert_Message.kt
+++ android_Practice_Problems/Display_Alert_Message.kt
@@ -1,2 +1,22 @@
 package practice_kotlin.android_Practice_Problems
 
+fun main() {
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId.
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}
